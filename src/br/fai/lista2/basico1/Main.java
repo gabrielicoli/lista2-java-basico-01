dc380abc09964e6bede1d74b0d9fae489ceac669
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-09 14:25:01 -0300

    primeiro commit

--- src/br/fai/lista2/basico1/Main.java
+++ src/br/fai/lista2/basico1/Main.java
@@ -1,10 +1,36 @@
 package br.fai.lista2.basico1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
 
 	}
 
-}
+	private void start() {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		Map mapaCandidatos = new HashMap(); 
+	    mapaCandidatos.put(1, "João");
+	    mapaCandidatos.put(2, "Maria");
+	    mapaCandidatos.put(3, "Jose");
+	    mapaCandidatos.put(4, "Antonio");
+	    mapaCandidatos.put(5, "Tiburssinho");
+	    
+	    
+	 
+			
+			
+		}
+	}
+
+
