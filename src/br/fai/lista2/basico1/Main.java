69c598e1ad168f58e7acb84da2ca5c93e170e320
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-10 10:03:47 -0300

    tentando fazer

--- src/br/fai/lista2/basico1/Main.java
+++ src/br/fai/lista2/basico1/Main.java
@@ -18,19 +18,43 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		
-		
-		Map mapaCandidatos = new HashMap(); 
+		Map<Integer, String> mapaCandidatos = new HashMap<>(); 
 	    mapaCandidatos.put(1, "João");
 	    mapaCandidatos.put(2, "Maria");
 	    mapaCandidatos.put(3, "Jose");
 	    mapaCandidatos.put(4, "Antonio");
 	    mapaCandidatos.put(5, "Tiburssinho");
 	    
+	    ArrayList<Integer> votosCandidatos= new ArrayList();
+	    votosCandidatos.add(0);
+	    votosCandidatos.add(0);
+	    votosCandidatos.add(0);
+	    votosCandidatos.add(0);
+	    votosCandidatos.add(0);
+	    
+	    for (Integer chaveCandidatos : mapaCandidatos.keySet()) {
+			int voto=-1;
+	    	do {
+	    	System.out.println("Digite seu voto: ");
+	    	voto = scanner.nextInt();
+	    	
+	    	if(voto == chaveCandidatos) {
+	    		votosCandidatos(chaveCandidatos);
+	    	}
+	    		
+	    	
+	    	}while(voto!=0);
+	    			}
 	    
 	 
 			
 			
 		}
+
+	private void votosCandidatos(Integer chaveCandidatos) {
+		// TODO Auto-generated method stub
+		
+	}
 	}
 
 
